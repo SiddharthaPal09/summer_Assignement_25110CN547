4a46e48ff3bac16ba832483d15da4f3aad768abd
Siddhartha Pal <siddharthapal8081@gmail.com>
2026-06-08 23:21:11 +0530

    Add DAY-10 pattern solutions

---
+++ DAY-10/Q38.java
@@ -0,0 +1,16 @@
+/*Write a program to Print reverse pyramid. */
+public class Q38 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ DAY-10/Q39.java
@@ -0,0 +1,18 @@
+/*Write a program to Print number pyramid. */
+public class Q39 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            char ch = (char) ('A' + i - 1);
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+            }
+
+            System.out.println();
+        }
+    }
+}
