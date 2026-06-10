23fe7ada9406ecc80ba6da19588fe3338b7dc394
Siddhartha Pal <siddharthapal8081@gmail.com>
2026-06-10 23:25:07 +0530

    DAY-13 function assignments

---
+++ DAY-13/Q49.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q49 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
