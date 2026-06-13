b7fc34ef72837fe890e28833b7a52e782e379833
Siddhartha Pal <siddharthapal8081@gmail.com>
2026-06-13 20:57:36 +0530

    DAY-16 function assignments

---
+++ DAY-16/Q61.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q61 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n - 1];
+
+        int sum = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        int expected = n * (n + 1) / 2;
+
+        System.out.println("Missing Number = " + (expected - sum));
+    }
+}
