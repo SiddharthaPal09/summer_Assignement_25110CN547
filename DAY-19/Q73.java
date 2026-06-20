81c1cb29590978d3a659edb052b04900c7597d37
Siddhartha Pal <siddharthapal8081@gmail.com>
2026-06-20 11:33:30 +0530

    Add DAY-19 matrix assignments

---
+++ DAY-19/Q73.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Q73 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int a[][] = new int[rows][cols];
+        int b[][] = new int[rows][cols];
+        int sum[][] = new int[rows][cols];
+
+        // Input first matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // Input second matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        // Add matrices
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        // Print result
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ DAY-19/Q76.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q76 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a[][] = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += a[i][i];
+        }
+
+        System.out.println("Diagonal Sum = " + sum);
+    }
+}
